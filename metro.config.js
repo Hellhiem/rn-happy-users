1cc0f8ad7a67c00846e2db9560e9348a3ca5545b
Milosz Szczepanek <milosz23wow@gmail.com>
2021-12-11 11:03:03 +0100

    Lock app to portrait mode & fix lint error

--- metro.config.js
+++ metro.config.js
@@ -5,11 +5,11 @@
  * @format
  */
 // eslint-disable-next-line
-const {getDefaultConfig} = require('metro-config');
+const { getDefaultConfig } = require('metro-config');
 
 module.exports = (async () => {
   const {
-    resolver: {sourceExts, assetExts},
+    resolver: { sourceExts, assetExts },
   } = await getDefaultConfig();
   return {
     transformer: {
@@ -22,7 +22,7 @@ module.exports = (async () => {
       }),
     },
     resolver: {
-      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      assetExts: assetExts.filter((ext) => ext !== 'svg'),
       sourceExts: [...sourceExts, 'svg'],
     },
   };
